1d6817a426073abdde455cf44e06783e6e43249a
gracebir <grace.devolop@gmail.com>
2022-12-15 16:36:07 +0200

    finish up the bdage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import styled from "styled-components";
+import Badge from "./components/Badge";
 import Navbar from "./components/Navbar";
 import Searchinput from "./components/Searchinput";
+import Seed4 from './assets/seed4.svg'
+import { badgedata } from "./data";
 
 const Main = styled.main`
   margin: 0 auto;
-  width: 50%;
+  width: 58%;
   padding: 1rem 0;
+  border: 1px red solid;
+`
+
+const Div = styled.div`
+display: flex;
+gap: 2%;
 `
 
 function App() {
@@ -13,9 +22,14 @@ function App() {
     <>
       <Navbar/>
       <Main>
-      <Searchinput/>
+        <Searchinput/>
+        <Div>
+          {badgedata.map((badge, i)=>(
+            <Badge Icon={badge.Icon} text={badge.text}/>
+          ))}
+        </Div>
+       
       </Main>
-      
     </>
   );
 }
--- src/data/index.tsx
+++ src/data/index.tsx
@@ -1,3 +1,9 @@
+import Seed from '../assets/seed.svg'
+import Seed1 from '../assets/seed1.svg'
+import Seed2 from '../assets/seed2.svg'
+import Seed3 from '../assets/seed3.svg'
+import Seed4 from '../assets/seed4.svg'
+
 export const navitems = [
     {
         item: 'Каталог продукції',
@@ -20,3 +26,27 @@ export const navitems = [
         id: 5
     }
 ]
+
+export const badgedata = [
+    {
+        Icon: Seed,
+        text: 'Насіння'
+    },
+    {
+        Icon: Seed1,
+        text: 'Засоби захисту рослин'
+    },
+    {
+        Icon: Seed2,
+        text: 'Добрива'
+    },
+    {
+        Icon: Seed3,
+        text: 'кормова група'
+    },
+    {
+        Icon: Seed4,
+        text: 'Агроному в поміч'
+    },
+]
+
