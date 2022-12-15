cfc5f3cf80dedc8448b01de4120a3292682358b7
gracebir <grace.devolop@gmail.com>
2022-12-15 14:39:27 +0200

    done with search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import styled from "styled-components";
 import Navbar from "./components/Navbar";
+import Searchinput from "./components/Searchinput";
+
+const Main = styled.main`
+  margin: 0 auto;
+  width: 50%;
+  padding: 1rem 0;
+`
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Main>
+      <Searchinput/>
+      </Main>
+      
     </>
   );
 }
