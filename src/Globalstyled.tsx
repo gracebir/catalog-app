fb747728b970b5c1263ac82c388dbc321acb5e58
gracebir <grace.devolop@gmail.com>
2022-12-15 10:56:37 +0200

    setting up font and uploading images

--- src/Globalstyled.tsx
+++ src/Globalstyled.tsx
@@ -6,4 +6,19 @@ export const Globalstyled = createGlobalStyle`
         padding: 0;
         box-sizing: border-box;
     }
+
+    :root{
+        --color-dark-grenn: #1E6140;
+        --color-green: #84C551;
+        --color-red: #E74E13;
+        --color-dark-red: #8D3200;
+        --color-black: #000000;
+        --color-white: #FFFFFF;
+        --color-cream: #F8F8F8;
+    }
+
+    body{
+        font-family: 'Montserrat', sans-serif;
+        background-color: var(--color-cream);
+    }
 `
